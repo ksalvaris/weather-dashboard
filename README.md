bacf1453e7ab5694d56bb3573605ef03ee4762aa
Pickled_Onion <ksalvaris@gmail.com>
2023-07-23 17:51:09 +0800

    added initial html and css

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # weather-dashboard
+
+## API key
+75f727c580049b6c9fecb8d9f5740286 
---
+++ script.js
@@ -0,0 +1,4 @@
+var APIKey = "75f727c580049b6c9fecb8d9f5740286"; 
+var city; 
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+
